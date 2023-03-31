62a5230d24f456d4ce8471a62586f431b981064a
Julius Tan <87463654+juliustan22@users.noreply.github.com>
2023-03-31 11:12:51 +0800

    Update Capstone_Streamlit_App.py

--- Capstone_Streamlit_App.py
+++ Capstone_Streamlit_App.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from math import sqrt
-from unidecode import unidecode
+# from unidecode import unidecode
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
